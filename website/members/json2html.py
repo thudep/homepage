d8af4565fdbe5bcd1107d92a55679c7e5c9c476b
Qhy23 <18137798371@163.com>
2025-08-24 21:51:13 +0800

    feat: 完善个人主页的设计
    css样式使用了deepseek的代码, 基本未经修改, 后续可能还要调整

--- website/members/json2html.py
+++ website/members/json2html.py
@@ -1,87 +1,70 @@
 import json
 from bs4 import BeautifulSoup
 
-def html_add_p(soup, section_tag, text: str) -> None:
-    '''
-    添加<p>对象
-    '''
-    p_tag = soup.new_tag('p')
-    p_tag.string = text
-    section_tag.append(p_tag)
-
-def html_add_pa(soup, section_tag, a, text: str) -> None:
-    '''
-    添加<p>对象, 内部为<a>
-    '''
-    p_tag = soup.new_tag('p')
-    p_tag.append(a)
-    p_tag.append(text)
-    section_tag.append(p_tag)
-
-def add_mail(soup, section_tag, mail: str) -> None:
-    '''
-    把邮箱信息作为链接添加<a>对象
-    '''
-    a = soup.new_tag("a", href=f"mailto:{mail}", **{"class": "conduct"})
-    i = soup.new_tag("i", **{"class": "fas fa-envelope"})
+def link(soup, href, icon):
+    "链接为 href 的 icon 图标"
+    a = soup.new_tag("a", href=href, target="_blank", **{"class": "conduct"})
+    i = soup.new_tag("i", **{"class": icon})
     a.append(i)
-    html_add_pa(soup, section_tag, a, f": {mail}")
+    return a
 
-def add_github(soup, section_tag, github: str) -> None:
-    '''
-    把github信息作为链接添加<a>对象
-    '''
-    a = soup.new_tag("a",
-                     href=f"https://github.com/{github}",
-                     target="_blank",
-                     **{"class": "conduct"})
-    i = soup.new_tag("i", **{"class": "fab fa-github"})
-    a.append(i)
-    html_add_pa(soup, section_tag, a, f": {github}")
-
-def add_position(soup, section_tag, member: dict) -> None:
-    '''
-    获取成员职务信息, 并作为文本添加<p>对象
-    '''
-    for s in range(1, 6):
-        if f'position{s}' not in member:
-            return
-        if member[f'position{s}'] == '主席':
-            html_add_p(soup, section_tag, 
-                       f"第{member[f'session{s}']}届主席")
-        else:
-            html_add_p(soup, section_tag, 
-                       f"第{member[f'session{s}']}届{member[f'department{s}']}{member[f'position{s}']}")
+def add_item(soup, div, title, content):
+    "添加 info-item 对象"
+    item = soup.new_tag("div", **{"class": "info-item"})
+    p = soup.new_tag("p")
+    span = soup.new_tag("span", **{"class": "info-title"})
+    if isinstance(title, list):
+        span.extend(title)
+    else:
+        span.append(title)
+    p.append(span)
+    p.append(content)
+    item.append(p)
+    div.append(item)
 
 def main():
     # 读取json
     with open('members.json', 'r', encoding='utf-8') as f:
         members = json.load(f)
-    
+
     # 遍历每个人, 生成对应的网页
     for member in members:
         # 读取 HTML 模板
         with open('member/template.html', 'r', encoding='utf-8') as f:
             soup = BeautifulSoup(f, 'html.parser')
-        # 创建 <section class="content">
-        section_tag = soup.new_tag('section', **{'class': 'content'})
-        # 添加基本信息
-        html_add_p(soup, section_tag, f"姓名: {member['name']}")
+
+        # 补充个人信息
+        div = soup.find(id="information")
+        add_item(soup, div, '姓名', member['name'])
         if 'class' in member:
-            html_add_p(soup, section_tag, f"班级: {member['class']}")
+            add_item(soup, div, '班级', member['class'])
         if 'mail' in member:
-            add_mail(soup, section_tag, member['mail'])
+            add_item(soup, div,
+                     ['邮箱', link(soup, f"mailto:{member['mail']}", "fas fa-envelope")],
+                     member['mail'])
         if 'GitHub' in member:
-            add_github(soup, section_tag, member['GitHub'])
-        # 添加职务信息
-        add_position(soup, section_tag, member)
-        # 添加工作信息
+            add_item(soup, div,
+                     ['GitHub',
+                      link(soup, f"https://github.com/{member['GitHub']}", 'fab fa-github'),
+                      '用户名'],
+                     member['GitHub'])
+        # 补充任职情况
+        div = soup.find(id="position")
+        for s in range(1, 6):
+            if f'position{s}' not in member:
+                break
+            if member[f'position{s}'] == '主席':
+                add_item(soup, div, f"第{member[f'session{s}']}届", '科协主席')
+            else:
+                add_item(soup, div, f"第{member[f'session{s}']}届",
+                        f"{member[f'department{s}']}{member[f'position{s}']}")
+        # 补充参与工作
+        div = soup.find(id="work")
         w = 1
         while f'work{w}' in member:
-            html_add_p(soup, section_tag, member[f'work{w}'])
+            add_item(soup, div, '', member[f'work{w}'])
             w += 1
-        # 插入到 <main> 最后
-        soup.main.append(section_tag)
+
         # 保存结果
         with open(f"member/{member['website']}.html", 'w', encoding='utf-8') as f:
             f.write(soup.prettify())
